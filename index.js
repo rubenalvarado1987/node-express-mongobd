a87b21770a6d870c4f74a28278e5f38cf88a27de
RUBEN ALVARADO <rubenalvarado@MacBook-Pro-de-RUBEN.local>
2020-01-08 15:55:40 -0300

    test 3 mongobd

--- index.js
+++ index.js
@@ -11,17 +11,13 @@ app.use(bodyparser.json());
 var router = express.Router();
 var Evento = require('./models/Evento');
 
-mongoose.connect(bd, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.openUri(bd, { useNewUrlParser: true, useUnifiedTopology: true });
 
-var db = mongoose.connection;
 
-var schema = new Schema({ aNumber: { type: Number, default: 4.815162342 }})
-var M = db.model('M', schema)
-var m = new M;
-console.log(m.aNumber)
+
 
 app.get('/', function (req, res) {
-  res.send(app_name + ' running'+ 'a numer db: ' +m.aNumber);
+  res.send(app_name + ' running');
 });
 
 app.listen(process.env.PORT, function () {
