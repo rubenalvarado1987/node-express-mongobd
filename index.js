318e454b2d2cfa8fe7b945efc40df79436033f78
RUBEN ALVARADO <rubenalvarado@MacBook-Pro-de-RUBEN.local>
2020-01-08 15:46:23 -0300

    pruebas mongo

--- index.js
+++ index.js
@@ -4,23 +4,24 @@ var mongoose = require('mongoose');
 var bodyparser = require('body-parser');
 var app = express();
 var app_name = require('./package.json').name + ' app';
+var bd = require('./package.json').bd;
 
 app.use(bodyparser.json());
 
 var router = express.Router();
 var Evento = require('./models/Evento');
 
-mongoose.connect(require('./package.json').bd, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(bd, { useNewUrlParser: true, useUnifiedTopology: true });
 
-router.get('/events', 1, (req, res) => {
-  Evento.find()
-  .exec()
-  .then(x => res.status(200)
-  .send(x))
-})
+var db = mongoose.connection;
+
+var schema = new Schema({ aNumber: { type: Number, default: 4.815162342 }})
+var M = db.model('M', schema)
+var m = new M;
+console.log(m.aNumber)
 
 app.get('/', function (req, res) {
-  res.send(app_name + ' running');
+  res.send(app_name + ' running'+ 'a numer db: ' +m.aNumber);
 });
 
 app.listen(process.env.PORT, function () {
