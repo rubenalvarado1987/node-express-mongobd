65d5d0e70d7dd8371e460ae5ce308f0f99e8eb1d
RUBEN ALVARADO <rubenalvarado@MacBook-Pro-de-RUBEN.local>
2020-01-08 15:06:26 -0300

    mongose

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 
 var express = require('express');
+var mongoose = require('mongoose');
 var app = express();
 var app_name = require('./package.json').name + ' app'
+
+mongoose.connect(require('./package.json').bd, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.get('/', function (req, res) {
   res.send(app_name + ' running');
